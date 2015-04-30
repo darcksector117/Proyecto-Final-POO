b011d1f59fbe1cfe4033e9b2f7d015618c7bd09b
darcksector117 <darcksector117@hotmail.com>
2015-04-30 09:45:11 -0500

    30/4 9:44
    Clase Baraja creada

---
+++ Blackjack/src/Baraja.java
@@ -0,0 +1,18 @@
+
+public class Baraja {
+
+	private Naipe[] baraja=new Naipe[52];
+	private int posicion;
+	
+	public Baraja(){
+		this.posicion=posicion;
+	}
+	
+	public void mezclar(){
+		
+	}
+	
+	public Naipe next(){
+		
+	}
+}
